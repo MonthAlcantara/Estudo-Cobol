bbf7f57e55e81b9650406a9087cc8853b0258cd8
monthalcantara <montival_junior@yahoo.com.br>
2020-10-14 18:19:48 -0300

    trabalhando com datas

---
+++ trabalhando-datas.cob
@@ -0,0 +1,21 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PROGCOB01.
+      ***************************************************
+      * AREA DE COMENTARIOS - REMARKS
+      * AUTHOR = MONTIVAL JUNIOR
+      * OBJETIVO = RECEBER E IMPRIMIR A DATA DO SISTEMA
+      * UTILIZAR AS VARIAVEIS NIVEL 01.02..(ESTRUTURADA)
+      * DATA = 14/10/2020
+      ***************************************************
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 WRK-DATA.
+           02 WRK-ANO PIC 9(04) VALUE ZEROS.
+           02 WRK-MES PIC 9(02) VALUE ZEROS.
+           02 WRK-DIA PIC 9(02) VALUE ZEROS.
+       77 WRK-NOME PIC X(20) VALUE SPACES.
+       PROCEDURE DIVISION.
+           ACCEPT WRK-DATA FROM DATE YYYYMMDD.
+           DISPLAY 'DATA = ' WRK-DIA ' DE ' WRK-MES ' DE ' WRK-ANO
+           STOP RUN.
