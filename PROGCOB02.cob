aa0d6aced6bd6c6776556e90c074bfb03c583d41
monthalcantara <montival_junior@yahoo.com.br>
2020-10-14 17:56:55 -0300

    recebendo e imprimindo variavel

---
+++ PROGCOB02.cob
@@ -0,0 +1,16 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PROGCOB01.
+      ***************************************************
+      * AREA DE COMENTARIOS - REMARKS
+      * AUTHOR = MONTIVAL JUNIOR
+      * OBJETIVO = RECEBER E IMPRIMIR UMA STRING
+      * DATA = 14/10/2020
+      ***************************************************
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       77 WRK-NOME PIC X(20) VALUE SPACES.
+       PROCEDURE DIVISION.
+           ACCEPT WRK-NOME FROM CONSOLE.
+           DISPLAY 'O VALOR DIGITADO NO CONSOLE FOI =' WRK-NOME.
+           STOP RUN.
