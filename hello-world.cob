0bf5caadf21911f4d2f8e3c29c8d1d211c964b74
monthalcantara <montival_junior@yahoo.com.br>
2020-10-14 18:20:28 -0300

    modificado nome dos arquivos

---
+++ hello-world.cob
@@ -0,0 +1,13 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PROGCOB01.
+      ***************************************************
+      * AREA DE COMENTARIOS - REMARKS
+      * AUTHOR = MONTIVAL JUNIOR
+      * OBJETIVO = MOSTRAR A STRING HELLO WORLD
+      * DATA = 14/10/2020
+      ***************************************************
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       PROCEDURE DIVISION.
+           DISPLAY 'HELLO WORLD'.
+           STOP RUN.
